0248e3fb9e3120d2da64a08c80e4d4103d09b38a
Dave LaCroix <davedalelacroix@gmail.com>
2018-12-15 12:40:50 -0500

    stuff

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,13 @@ var wins = 0;
 var losses = 0;
 var guesses = 7;
 var guessesLeft = 7;
-var guessedLetters = [];
-var letterToGuess = null;
+var guess = [];
+var letterleft = null;
 
 var cpuChoices = ["a", "b", "c", "d", "e","f","g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var cpuGuess = cpuChoices[Math.floor(Math.random()*cpuChoices.length)];
     console.log(cpuGuess);
+
+    document.onkeyup = function(event) {
+        var letter = event.key.toLowerCase();
